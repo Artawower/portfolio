51471adc6bbc00b610d09c0dbc44603d2e8b556d
artawower <artawower@mail.ru>
2022-10-08 14:37:59 +0400

    Updates

---
+++ assets/ContactPage.3ee8def0.js
@@ -0,0 +1,1 @@
+import{o as t}from"./index.0f9081e3.js";const o=()=>t("div",{children:t("h1",{children:"Contact Page"})});export{o as default};
--- assets/ContactPage.51bddfea.js
+++
@@ -1,1 +0,0 @@
-import{o as t}from"./index.92d06ebe.js";const o=()=>t("div",{children:t("h1",{children:"Contact Page"})});export{o as default};
